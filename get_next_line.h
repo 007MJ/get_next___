bc1e869e8daefb717edc376a6aab79ce21050451
007MJ <k.mijiza@gmail.com>
2023-02-16 19:32:58 -0500

    add: function rewrite on the static

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mnshimiy <mnshimiy@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/16 19:05:12 by mnshimiy          #+#    #+#             */
+/*   Updated: 2023/02/16 19:12:54 by mnshimiy         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#	ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+
+
+#endif
--- nextline.c
+++ nextline.c
@@ -18,21 +18,53 @@ int	ft_strlen(char *str)
 
 char	*ft_concante(char *dst, char *src);
 
-char	*ft_strmalloc(int size)
+void	*ft_strcalloc(int count, size_t size)
 {
-	
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = malloc (count * sizeof(size));
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		str[i] = 0;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_backslah(char *str)
+{
+	char	*strslah;
+	int		i;
+
+	i = 0;
+	strslah = ft_strcalloc(ft_strlen(str), sizeof(char));
+	while (str[i - 1] != '\n')
+	{
+		strslah[i] = str[i];
+		i++;
+	}
+	str = ft_onstatic(str, str + i);
+	return (strslah);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*dst;
 	char		*src;
+	char		*temp;
 
-	src = ft_strmalloc(BUFFER_SIZE);
+	src = ft_strcalloc(BUFFER_SIZE, sizeof(char));
 	read(fd, src, BUFFER_SIZE);
-	dst = ft_concante(dst, src);
-	return (dst);
+	temp = ft_concante(dst, src);
+	dst = temp;
+	ft_backslah(ft_backslah(dst))
+	return ();
 }
+
 
 char	*ft_concante(char *dst, char *src)
 {
@@ -42,7 +74,7 @@ char	*ft_concante(char *dst, char *src)
 
 	i = 0;
 	j = 0;
-	tmp = ft_strmalloc(ft_strlen(src) + ft_strlen(dst) + 1);
+	tmp = ft_strcalloc(ft_strlen(src) + ft_strlen(dst) + 1, sizeof(char));
 	if (dst != NULL)
 	{
 		while (dst[i])
